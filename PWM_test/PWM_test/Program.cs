1be15703abbadba7abbd162466ea8eacf61fe5f8
nicolaspschd <nicolas.pschd@eduge.ch>
2017-03-03 09:27:08 +0100

    Ca tourne dans le sens qu'on veut

--- PWM_test/PWM_test/Program.cs
+++ PWM_test/PWM_test/Program.cs
@@ -10,11 +10,21 @@ namespace PWM_test
     {
         public static void Main()
         {
-            //OutputPort dir = new OutputPort(FEZSpider.Socket8.Pwm7, true);
-            double frequence = 100000; // Période en microseconde
-            double rapportCyclique = 0.50; // Période en microseconde
-            PWM motorDriver = new PWM(FEZSpider.Socket8.Pwm9, frequence, rapportCyclique, false);
-            motorDriver.Start();
+            //OutputPort dir = new OutputPort(FEZSpider.Socket8.Pin9, true);
+            double frequence = 38000; // Période en microseconde
+            double rapportCyclique = 0.5; // Période en microseconde
+            PWM motorDriver = new PWM(FEZSpider.Socket8.Pwm7, frequence, rapportCyclique, false);
+            motorDriver.Stop();
+            int i = 0;
+            /*while (true)
+            {
+                i++;
+                if (i >= 100000)
+                {
+                    dir.Write(!dir.Read());
+                    i = 0;
+                }
+            }*/
         }
     }
 }
