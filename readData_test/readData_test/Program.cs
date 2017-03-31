c68e6e51615222ce588c8cb4cf31b106e8fce27f
nicolaspschd <nicolas.pschd@eduge.ch>
2017-03-31 11:24:28 +0200

    UART + Interrupt port

--- readData_test/readData_test/Program.cs
+++ readData_test/readData_test/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.SPOT;
 using System.Text;
+using System.Threading;
 using System.IO.Ports;
 
 namespace readData_test
@@ -9,16 +10,43 @@ namespace readData_test
     {
         public static void Main()
         {
-
-            SerialPort UART = new SerialPort("COM1", 115200);
-            int read_count = 0;
+            SerialPort UART = new SerialPort("COM3", 115200);
             byte[] rx_byte = new byte[1];
             UART.Open();
-            read_count = UART.Read(rx_byte, 0, 1);
 
-            for (int i = 0; i < read_count; i++)
+            while (true)
             {
-                Debug.Print(UART.Read(rx_byte, 0, 1).ToString());
+                // Lecture du byte              
+                if (UART.Read(rx_byte, 0, 1) > 0)// do we have data?  
+                {
+                    string counter_string = rx_byte[0].ToString();
+                    // Converti la chaine en bytes                
+                    byte[] buffer = Encoding.UTF8.GetBytes(counter_string);
+                    string output = string.Empty;
+
+                    foreach (char item in UTF8Encoding.UTF8.GetChars(buffer))
+                    {
+                        output += item;
+                    }
+
+                    switch (output)
+                    {
+                        case "49":
+                            output = "1";
+                            break;
+                        case "50":
+                            output = "2";
+                            break;
+                        case "51":
+                            output = "3";
+                            break;
+                        case "115":
+                            output = "stop";
+                            break;
+                    }
+
+                    Debug.Print(output);
+                }
             }
         }
     }
