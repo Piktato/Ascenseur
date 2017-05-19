19e65219074e893214232b25a59f4443c7c2674c
Piktato <ardi.rmsh@eduge.ch>
2017-05-19 11:28:32 +0200

    commentaire ascCommandes

--- command_asc/command_asc/Program.cs
+++ command_asc/command_asc/Program.cs
@@ -1,3 +1,11 @@
+/*
+ *  Auteur : Paschoud Nicolas, Ramushi Ardi
+ *  Projet : command_asc
+ *  Description : Envoie les commandes de l'ascenseur d'une carte à l'autre
+ *  Date : 27 janvier 2017
+ *  Version : 1.0
+ */
+
 using System;
 using Microsoft.SPOT;
 using GHI.Glide;
@@ -15,12 +23,14 @@ namespace command_asc
     {
         public static Window windows;
 
+        //  Port de communication avec l'autre carte
         public static SerialPort UART = new SerialPort("COM3", 115200);
-
+        
         public static void Main()
         {
             UART.Open();
 
+            //  Création de l'interface graphique sur l'écran
             windows = GlideLoader.LoadWindow(Resources.GetString(Resources.StringResources.windows));
 
             GlideTouch.Initialize();
@@ -35,9 +45,10 @@ namespace command_asc
             stopBtn.TapEvent += stop;
 
             Glide.MainWindow = windows;
-            Thread.Sleep(-1);
+            Thread.Sleep(Timeout.Infinite);
         }
 
+        // Envois les données à l'autre carte
         private static void sendCommand(string command)
         {
             byte[] dataSend = Encoding.UTF8.GetBytes(command);
@@ -45,12 +56,14 @@ namespace command_asc
             Debug.Print("Send !");
         }
 
+        // Evénement clique sur les boutton d'étage
         private static void OnTap(object sender)
         {
             Debug.Print("Etage : " + (sender as Button).Name);
             sendCommand((sender as Button).Name);
         }
 
+        // Evénement clique sur le bouton stop
         private static void stop(object sender)
         {
             Debug.Print("Stop");
